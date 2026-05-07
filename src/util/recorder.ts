f00a020fa0f6f53226d3fa4f941e8461e5cd773c
aponb <apre@gmx.at>
2026-05-07 17:27:04 +0200

    Add flag to disable deduplication

--- src/util/recorder.ts
+++ src/util/recorder.ts
@@ -168,6 +168,7 @@ export class Recorder extends EventEmitter {
   pageSeed?: ScopedSeed;
   pageSeedDepth = 0;
   dedupePagesMinDepth = -1;
+  dedupeEnabled = true;
 
   frameIdToExecId: Map<string, number> | null;
 
@@ -191,7 +192,10 @@ export class Recorder extends EventEmitter {
 
     this.shouldSaveStorage = !!crawler.params.saveStorage;
 
-    this.dedupePagesMinDepth = crawler.params.dedupePagesMinDepth;
+    this.dedupeEnabled = crawler.params.dedupe;
+    this.dedupePagesMinDepth = this.dedupeEnabled
+      ? crawler.params.dedupePagesMinDepth
+      : -1;
 
     this.writer = writer;
 
@@ -1777,7 +1781,7 @@ export class Recorder extends EventEmitter {
 
     let origRecSize = 0;
 
-    if (!isEmpty && url) {
+    if (this.dedupeEnabled && !isEmpty && url) {
       const res = await this.crawlState.getHashDupe(hash);
 
       // if only writing revisit, ensure it's a revisit and hash
@@ -1844,7 +1848,9 @@ export class Recorder extends EventEmitter {
 
     const addStatsCallback = async (size: number) => {
       try {
-        await this.crawlState.addHashNew(hash, url, date, size, origRecSize);
+        if (this.dedupeEnabled) {
+          await this.crawlState.addHashNew(hash, url, date, size, origRecSize);
+        }
         if (url) {
           const domain = this.crawler.getAttributedDomain(url, this.pageSeedId);
           if (domain) {
